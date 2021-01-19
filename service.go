6db6cb78888629e1ba9f73d5be48d18e1f5ac488
Alexey Sachko <asachko@gmini.tech>
2021-01-19 14:55:39 +0500

    added ACL handling for unary methods

--- service.go
+++ service.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"net"
+	"strings"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 )
 
 // тут вы пишете код
@@ -27,6 +30,81 @@ func NewAdminService() AdminServer {
 	return service
 }
 
+type bizService struct {
+}
+
+func (bz *bizService) Check(ctx context.Context, n *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (bz *bizService) Add(ctx context.Context, n *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (bz *bizService) Test(ctx context.Context, n *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+func NewBizService() BizServer {
+	service := &bizService{}
+	return service
+}
+
+func authInterceptor(acl map[string][]string) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		// start := time.Now()
+
+		md, _ := metadata.FromIncomingContext(ctx)
+
+		consumers := md.Get("consumer")
+		if len(consumers) == 0 {
+			return nil, grpc.Errorf(codes.Unauthenticated, "Unauthenticated")
+		}
+
+		cons := consumers[0]
+		consMethods, ok := acl[cons]
+		if !ok {
+			return nil, grpc.Errorf(codes.Unauthenticated, "Unauthenticated")
+		}
+
+		allowed := false
+		for _, m := range consMethods {
+			// fmt.Println("allowed calc: ", info.FullMethod, m)
+
+			if strings.Contains(m, "/*") {
+				if strings.Contains(info.FullMethod, strings.Replace(m, "/*", "", -1)) {
+					allowed = true
+					break
+				}
+			} else if info.FullMethod == m {
+				allowed = true
+				break
+			}
+		}
+
+		if !allowed {
+			return nil, grpc.Errorf(codes.Unauthenticated, "Unauthenticated")
+		}
+
+		reply, err := handler(ctx, req)
+
+	// 	fmt.Printf(`
+	// info.FullMethod = %v
+	// req = %#v
+	// reply = %#v
+	// time = %v
+	// md = %v
+	// err = %v
+	// `, info.FullMethod, req, reply, time.Since(start), md, err)
+
+		return reply, err
+	}
+}
+
 func StartMyMicroservice(ctx context.Context, addr string, ACLData string) error {
 	acl := make(map[string][]string)
 	if err := json.Unmarshal([]byte(ACLData), &acl); err != nil {
@@ -38,8 +116,11 @@ func StartMyMicroservice(ctx context.Context, addr string, ACLData string) error
 		return err
 	}
 
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(authInterceptor(acl)),
+	)
 	RegisterAdminServer(server, NewAdminService())
+	RegisterBizServer(server, NewBizService())
 
 	go server.Serve(lis)
 
